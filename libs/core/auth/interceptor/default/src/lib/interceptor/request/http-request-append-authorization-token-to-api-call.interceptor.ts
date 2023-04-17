936452e93f5b57302cfe5d9d446ff419993ebe0c
Franklin RD <>
2023-04-17 22:38:58 +0200

    Update http-request-append-authorization-token-to-api-call.interceptor.ts

--- libs/core/auth/interceptor/default/src/lib/interceptor/request/http-request-append-authorization-token-to-api-call.interceptor.ts
+++ libs/core/auth/interceptor/default/src/lib/interceptor/request/http-request-append-authorization-token-to-api-call.interceptor.ts
@@ -35,39 +35,46 @@ export class HttpRequestAppendAuthorizationTokenToApiCallInterceptor
 
     // add authorization header in api request with jwt token if available
 
-    if (!this.apiCallsPrefix || this.apiCallsPrefix.length == 0) {
-      return next.handle(request);
+    if (!request.headers.has('Authorization')) {
+      if (!this.apiCallsPrefix || this.apiCallsPrefix.length == 0) {
+        return next.handle(request);
+      }
+
+      const find = this.apiCallsPrefix.find(
+        (prefix: string) => request.url.indexOf(prefix) !== -1
+      );
+
+      const logged: boolean = this.store.selectSnapshot<boolean>(
+        (state) => state.auth?.logged
+      );
+
+      if (!find || !logged) {
+        return next.handle(request);
+      }
+
+      this.logger.console.debug(
+        this.__classname,
+        'find && logged',
+        find,
+        logged
+      );
+
+      const accessToken: any = this.store.selectSnapshot<any>(
+        (state) => state.auth?.token?.accessToken
+      );
+
+      if (!accessToken) {
+        return next.handle(request);
+      }
+
+      // this.logger.console.debug(this.__classname, 'accessToken', accessToken);
+
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
     }
-
-    const find = this.apiCallsPrefix.find(
-      (prefix: string) => request.url.indexOf(prefix) !== -1
-    );
-
-    const logged: boolean = this.store.selectSnapshot<boolean>(
-      (state) => state.auth?.logged
-    );
-
-    if (!find || !logged) {
-      return next.handle(request);
-    }
-
-    this.logger.console.debug(this.__classname, 'find && logged', find, logged);
-
-    const accessToken: any = this.store.selectSnapshot<any>(
-      (state) => state.auth?.token?.accessToken
-    );
-
-    if (!accessToken) {
-      return next.handle(request);
-    }
-
-    // this.logger.console.debug(this.__classname, 'accessToken', accessToken);
-
-    request = request.clone({
-      setHeaders: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
 
     if (!request.headers.has('Content-Type')) {
       request = request.clone({
